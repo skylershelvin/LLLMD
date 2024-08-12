d3e21f6e39eb4c5889ee3d83be41ada95b3e0edc
Ryan Heron <rwh60@txstate.edu>
2024-08-12 16:59:27 -0500

    spring initializer'd

---
+++ src/main/java/com/revature/LLL/LllApplication.java
@@ -0,0 +1,13 @@
+package com.revature.LLL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LllApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LllApplication.class, args);
+	}
+
+}
