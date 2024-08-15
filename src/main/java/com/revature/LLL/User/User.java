76bb5b51c06e0590a7ea9f277145f8c43c4362b3
Luis-Cruz1027 <luisalejandrocruz1027@gmail.com>
2024-08-15 14:56:01 -0500

    quick comment

--- src/main/java/com/revature/LLL/User/User.java
+++ src/main/java/com/revature/LLL/User/User.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Table(name="users")
+@Table(name="users") //users table
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
