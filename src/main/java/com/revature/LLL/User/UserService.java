c7c1048ce13eba1926c4131b608d6d39974512c9
Luis-Cruz1027 <luisalejandrocruz1027@gmail.com>
2024-08-15 14:44:35 -0500

    user model, user controller, user repo, and user service with utility interface

---
+++ src/main/java/com/revature/LLL/User/UserService.java
@@ -0,0 +1,42 @@
+package com.revature.LLL.User;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.revature.LLL.util.interfaces.Serviceable;
+
+import java.util.List;
+
+@Service
+public class UserService implements Serviceable<User> {
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public List<User> findAll() {
+        return List.of();
+    }
+
+    @Override
+    public User create(User newObject) {
+        return null;
+    }
+
+    @Override
+    public User findById(int id) {
+        return null;
+    }
+
+    @Override
+    public Boolean update(User updatedObject) {
+        return null;
+    }
+
+    @Override
+    public Boolean delete(User deletedObject) {
+        return null;
+    }
+}
