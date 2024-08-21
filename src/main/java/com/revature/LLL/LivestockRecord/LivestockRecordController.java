4fd95c907c3bb6f0a03d837957a74b59af2bd3fd
Connie Angeley <connieangeley@gmail.com>
2024-08-21 13:40:14 -0500

    implemented getLivestockRecords. Needs tests

--- src/main/java/com/revature/LLL/LivestockRecord/LivestockRecordController.java
+++ src/main/java/com/revature/LLL/LivestockRecord/LivestockRecordController.java
@@ -1,9 +1,37 @@
 package com.revature.LLL.LivestockRecord;
 
+import com.revature.LLL.LivestockMedicalRecord.LivestockMedicalRecordService;
 import jakarta.persistence.*;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+@RestController
+@RequestMapping("medicalRecord")
 public class LivestockRecordController {
+    private final LivestockRecordService livestockRecordService;
+
+    @Autowired
+    public LivestockRecordController(LivestockRecordService livestockRecordService) {
+        this.livestockRecordService = livestockRecordService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<LivestockRecord>> getLivestockRecords(@Valid @RequestHeader int userId) {
+        if (userId == 0) {
+            return ResponseEntity.badRequest().build();
+        } else {
+            return ResponseEntity.ok(livestockRecordService.getLivestockRecords(userId));
+        }
+    }
+
 }
--- src/main/java/com/revature/LLL/LivestockRecord/LivestockRecordService.java
+++ src/main/java/com/revature/LLL/LivestockRecord/LivestockRecordService.java
@@ -1,9 +1,23 @@
 package com.revature.LLL.LivestockRecord;
 
+import com.revature.LLL.LivestockMedicalRecord.LivestockMedicalRecordRepository;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service
 public class LivestockRecordService {
+    private final LivestockRecordRepository livestockRecordRepository;
+
+    public LivestockRecordService(LivestockRecordRepository livestockRecordRepository){
+        this.livestockRecordRepository = livestockRecordRepository;
+    }
+
+    public List<LivestockRecord> getLivestockRecords(int userId) {
+        return livestockRecordRepository.findAllByOwner_UserId(userId);
+    }
 }
