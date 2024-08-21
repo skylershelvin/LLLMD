0a82f40e9ad979cc0958d10af8b44552b493f7e7
Connie Angeley <connieangeley@gmail.com>
2024-08-21 15:02:25 -0500

    Removed old LivestockMedicalRecord folder

--- src/main/java/com/revature/LLL/LivestockMedicalRecord/LivestockMedicalRecordRepository.java
+++
@@ -1,8 +0,0 @@
-package com.revature.LLL.LivestockMedicalRecord;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LivestockMedicalRecordRepository extends JpaRepository<LivestockMedicalRecord, Integer> {
-}
